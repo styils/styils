fcad47d474f763fd94ece08b49aa67f0945dfddb
zoy <zoy-l@outlook.com>
2022-07-20 11:22:30 +0800

    ci: updata docs sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env sh
 
-set -e
-
-npm run build:doc
-
 cd docs/dist
 
+git config --global user.name "github-actions[bot]"
+git config --global user.email "zoy-l@outlook.com"
 git add -A
 git commit -m 'update docs'
 
