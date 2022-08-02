c05be55cd7ce244e89282b73d70180b9553c3025
zoy <zoy-l@outlook.com>
2022-08-02 17:11:06 +0800

    chore: remove examples

--- examples/remix/app/theme.ts
+++
@@ -1,9 +0,0 @@
-import { createSystem } from '@styils/react'
-
-export const { styled, SystemProvider, getCssValue } = createSystem({
-  theme(mode) {
-    return {
-      color: mode === 'light' ? 'red' : 'blue'
-    }
-  }
-})
