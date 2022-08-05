5ab11e38f12824ab3806c909c61f521eb1bd3846
zoy <zoy-l@outlook.com>
2022-08-05 16:29:16 +0800

    fix(type): type errors

--- src/solidSystemTypes.ts
+++ src/solidSystemTypes.ts
@@ -3,28 +3,32 @@ import type { CSSAttribute } from 'nativeCssTypes'
 import type { Widen } from './types'
 import type { StyleCSSAttribute, StyleInterpolation } from './baseSystemTypes'
 
-export type StyledComponent = keyof JSX.IntrinsicElements | ((...props: any[]) => JSX.Element)
+export type NativeComponent = keyof JSX.IntrinsicElements | ((...props: any[]) => JSX.Element)
 
 type PropsWithRef<P> = 'ref' extends keyof P ? (P extends { ref?: infer R | undefined } ? R : P) : P
 
-type StyledProps<As extends StyledComponent, Variants> = Omit<ComponentProps<As>, 'ref'> & {
+type StyledProps<As extends NativeComponent, Variants> = Omit<ComponentProps<As>, 'ref'> & {
   ref?: PropsWithRef<ComponentProps<As>>
 } & {
-  as?: As
+  as?: As extends StyledComponent<infer A, any> ? A : As
   variants?: {
     [key in keyof Variants]?: Widen<keyof Variants[key]>
   }
 }
 
+type StyledComponent<Component extends NativeComponent, Variants> = <
+  As extends NativeComponent = Component
+>(
+  props: StyledProps<As, Variants>
+) => JSX.Element
+
 export interface Styled<Theme> {
   <
-    Component extends StyledComponent,
+    Component extends NativeComponent,
     Variants extends Record<string, Record<string, CSSAttribute>>
   >(
     component: Component | { tag: Component; namespace?: string },
     styles: StyleCSSAttribute<Theme>,
     interpolation?: StyleInterpolation<Theme, Variants>
-  ): {
-    <As extends StyledComponent = Component>(props: StyledProps<As, Variants>): JSX.Element
-  }
+  ): StyledComponent<Component, Variants>
 }
